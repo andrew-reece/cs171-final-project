aa559a354128d7710504b40e05a8e1f22c4ca499
Brian Feeny <brianfeeny@fas.harvard.edu>
2014-04-02 12:56:34 -0400

    got xhr working in .defer() but meters clobber each other, troubleshooting

--- loadData/loadData.js
+++ loadData/loadData.js
@@ -26,7 +26,7 @@ var svg = d3.select("body").append("svg")
 var meter = svg.append("g")
     .attr("class", "progress-meter");
 
-meter.append("path")
+var background = meter.append("path")
     .attr("class", "background")
     .attr("d", arc.endAngle(twoPi));
 
@@ -38,7 +38,7 @@ var text = meter.append("text")
     .attr("dy", ".35em");
     
 /*
-var myXhr = d3.csv("data/WLAN2.csv")
+var myXhr = d3.csv("../data/WLAN2.csv")
     .on("progress", function() {
       var i = d3.interpolate(progress, d3.event.loaded / d3.event.total);
       d3.transition().tween("progress", function() {
@@ -58,7 +58,6 @@ var myXhr = d3.csv("data/WLAN2.csv")
 // Need to be commented out in queue() and initVis.
 var transformData = function(
                       error
-/*
                       ,subjects
                       ,activities
                       ,calls
@@ -68,10 +67,9 @@ var transformData = function(
                       ,musicgenreimmersion
                       ,musicgenrepreference
                       ,politics
-                      ,proximity
+ //                     ,proximity
                       ,relationshipsfromsurveys
                       ,sms
-*/
                       ,wlan2
                       ) {
   if(error) {
@@ -84,7 +82,6 @@ var transformData = function(
 
 // Various transformations to our data
 
-/*
 //  console.log("Subjects", subjects);
   console.log("Transforming Activities...");
   activities.map(function(d) {
@@ -127,11 +124,13 @@ var transformData = function(
     d["survey.month"] = YmParser(d["survey.month"]);
   })
 //  console.log("Politics", politics);
+/*
   console.log("Transforming Proximity...");
   proximity.map(function(d) {
     d["time"] = YmdXParser(d["time"]);
   });
   console.log("Proximity", proximity);
+*/
   console.log("Transforming RelationshipsFromSurveys...");
   relationshipsfromsurveys.map(function(d) {
     d["survey.date"] = YmdParser(d["survey.date"]);
@@ -142,7 +141,6 @@ var transformData = function(
     d["time"] = YmdXParser(d["time"]);
   })
 //  console.log("SMS", sms);
-*/
   console.log("Transforming WLAN2...");
   wlan2.map(function(d) {
     d["time"] = YmdXParser(d["time"]);
@@ -156,7 +154,7 @@ var transformData = function(
 // It's not, whether pulling by itself or in the queue, or even making a program that only pulls Proximity into a CSV, it causes
 // the rest of the program to not function
 function loadProximity() {
-  d3.csv("data/Proximity2.csv", function(error, data) {
+  d3.csv("../data/Proximity2.csv", function(error, data) {
     if (error) {
       document.write("Error reading file");
       console.warn(error);
@@ -174,7 +172,7 @@ function loadProximity() {
     console.log("Proximity", proximity);
         
   }).on("progress", function(event) {
-      console.log("Loading data/Proximity.csv: ", formatPercent(d3.event.loaded/d3.event.total));
+      console.log("Loading ../data/Proximity.csv: ", formatPercent(d3.event.loaded/d3.event.total));
   });
 }
 
@@ -182,50 +180,172 @@ function loadProximity() {
 // based on http://bl.ocks.org/mbostock/3750941
 function loadData() {
   
-   queue()
-/*
-  .defer(d3.csv("data/Subjects.csv")
-         .on("progress", function() { console.log("Loading data/Subjects.csv: ",this.parent, formatPercent(d3.event.loaded/d3.event.total)); })                                           
-         .get, "error")
-  .defer(d3.csv("data/Activities.csv")
-          .on("progress", function() { console.log("Loading data/Activities.csv: ", formatPercent(d3.event.loaded/d3.event.total)); })                                           
-         .get, "error")
-  .defer(d3.csv("data/Calls.csv")
-         .on("progress", function() { console.log("Loading data/Calls.csv: ", formatPercent(d3.event.loaded/d3.event.total)); })                                           
-         .get, "error")
-  .defer(d3.csv("data/FluSymptoms.csv")
-         .on("progress", function() { console.log("Loading data/FluSymptoms.csv: ", formatPercent(d3.event.loaded/d3.event.total)); })                                           
-         .get, "error")
-  .defer(d3.csv("data/Health.csv")
-         .on("progress", function() { console.log("Loading data/Health.csv: ", formatPercent(d3.event.loaded/d3.event.total)); })                                           
-         .get, "error")
-  .defer(d3.csv("data/MusicGenreAwareness.csv")
-         .on("progress", function() { console.log("Loading data/MusicGenreAwareness.csv: ", formatPercent(d3.event.loaded/d3.event.total)); })                                           
-         .get, "error")
-  .defer(d3.csv("data/MusicGenreImmersion.csv")
-         .on("progress", function() { console.log("Loading data/MusicGenreImmersion.csv: ", formatPercent(d3.event.loaded/d3.event.total)); })                                           
-         .get, "error")
-  .defer(d3.csv("data/MusicGenrePreference.csv")
-         .on("progress", function() { console.log("Loading data/MusicGenrePreference.csv: ", formatPercent(d3.event.loaded/d3.event.total)); })                                           
-         .get, "error")
-  .defer(d3.csv("data/Politics.csv")
-         .on("progress", function() { console.log("Loading data/Politics.csv: ", formatPercent(d3.event.loaded/d3.event.total)); })                                           
-         .get, "error")
-  .defer(d3.csv("data/Proximity.csv")
-         .on("progress", function() { console.log("Loading data/Proximity.csv: ", formatPercent(d3.event.loaded/d3.event.total)); })                                           
-         .get, "error")
-  .defer(d3.csv("data/RelationshipsFromSurveys.csv")
-         .on("progress", function() { console.log("Loading data/RelationshipsFromSurveys.csv: ", formatPercent(d3.event.loaded/d3.event.total)); })                                           
-         .get, "error")
-  .defer(d3.csv("data/SMS.csv")
-         .on("progress", function() { console.log("Loading data/SMS.csv: ", formatPercent(d3.event.loaded/d3.event.total)); })                                           
-         .get, "error") 
+   queue(1)
 
-  .defer(d3.csv("data/WLAN2.csv")
-         .on("progress", function() { console.log("Loading data/WLAN2.csv: ", formatPercent(d3.event.loaded/d3.event.total)); })                                            
-         .get) */
+.defer(function(f) { d3.csv("../data/Subjects.csv")
+    .on("progress", function() {
+      var i = d3.interpolate(progress, d3.event.loaded / d3.event.total);
+      d3.transition().tween("progress", function() {
+        return function(t) {
+          progress = i(t);
+          background.attr("d", arc.endAngle(twoPi));
+          foreground.attr("d", arc.endAngle(twoPi * progress));
+          text.text(formatPercent(progress));
+        };
+      });
+    })
+    .get(function(error, data) {
+//      meter.transition().delay(250).attr("transform", "scale(0)");
+      f(error, data);
+    })
+})
+
+.defer(function(f) { d3.csv("../data/Activities.csv")
+    .on("progress", function() {
+      var i = d3.interpolate(progress, d3.event.loaded / d3.event.total);
+      d3.transition().tween("progress", function() {
+        return function(t) {
+          progress = i(t);
+          background.attr("d", arc.endAngle(twoPi));
+          foreground.attr("d", arc.endAngle(twoPi * progress));
+          text.text(formatPercent(progress));
+        };
+      });
+    })
+    .get(function(error, data) {
+//      meter.transition().delay(250).attr("transform", "scale(0)");
+      f(error, data);
+    })
+})
+
+.defer(function(f) { d3.csv("../data/Calls.csv")
+    .on("progress", function() {
+      var i = d3.interpolate(progress, d3.event.loaded / d3.event.total);
+      d3.transition().tween("progress", function() {
+        return function(t) {
+          progress = i(t);
+          background.attr("d", arc.endAngle(twoPi));
+          foreground.attr("d", arc.endAngle(twoPi * progress));
+          text.text(formatPercent(progress));
+        };
+      });
+    })
+    .get(function(error, data) {
+//      meter.transition().delay(250).attr("transform", "scale(0)");
+      f(error, data);
+    })
+})
+
+.defer(function(f) { d3.csv("../data/FluSymptoms.csv")
+    .on("progress", function() {
+      var i = d3.interpolate(progress, d3.event.loaded / d3.event.total);
+      d3.transition().tween("progress", function() {
+        return function(t) {
+          progress = i(t);
+          background.attr("d", arc.endAngle(twoPi));
+          foreground.attr("d", arc.endAngle(twoPi * progress));
+          text.text(formatPercent(progress));
+        };
+      });
+    })
+    .get(function(error, data) {
+//      meter.transition().delay(250).attr("transform", "scale(0)");
+      f(error, data);
+    })
+})
+
+.defer(function(f) { d3.csv("../data/Health.csv")
+    .on("progress", function() {
+      var i = d3.interpolate(progress, d3.event.loaded / d3.event.total);
+      d3.transition().tween("progress", function() {
+        return function(t) {
+          progress = i(t);
+          background.attr("d", arc.endAngle(twoPi));
+          foreground.attr("d", arc.endAngle(twoPi * progress));
+          text.text(formatPercent(progress));
+        };
+      });
+    })
+    .get(function(error, data) {
+//      meter.transition().delay(250).attr("transform", "scale(0)");
+      f(error, data);
+    })
+})
+
+.defer(function(f) { d3.csv("../data/MusicGenreAwareness.csv")
+    .on("progress", function() {
+      var i = d3.interpolate(progress, d3.event.loaded / d3.event.total);
+      d3.transition().tween("progress", function() {
+        return function(t) {
+          progress = i(t);
+          background.attr("d", arc.endAngle(twoPi));
+          foreground.attr("d", arc.endAngle(twoPi * progress));
+          text.text(formatPercent(progress));
+        };
+      });
+    })
+    .get(function(error, data) {
+//      meter.transition().delay(250).attr("transform", "scale(0)");
+      f(error, data);
+    })
+})        
+
+.defer(function(f) { d3.csv("../data/MusicGenreImmersion.csv")
+    .on("progress", function() {
+      var i = d3.interpolate(progress, d3.event.loaded / d3.event.total);
+      d3.transition().tween("progress", function() {
+        return function(t) {
+          progress = i(t);
+          background.attr("d", arc.endAngle(twoPi));
+          foreground.attr("d", arc.endAngle(twoPi * progress));
+          text.text(formatPercent(progress));
+        };
+      });
+    })
+    .get(function(error, data) {
+//      meter.transition().delay(250).attr("transform", "scale(0)");
+      f(error, data);
+    })
+})
          
-.defer(function(f) { d3.csv("data/WLAN2.csv")
+.defer(function(f) { d3.csv("../data/MusicGenrePreference.csv")
+    .on("progress", function() {
+      var i = d3.interpolate(progress, d3.event.loaded / d3.event.total);
+      d3.transition().tween("progress", function() {
+        return function(t) {
+          progress = i(t);
+          background.attr("d", arc.endAngle(twoPi));
+          foreground.attr("d", arc.endAngle(twoPi * progress));
+          text.text(formatPercent(progress));
+        };
+      });
+    })
+    .get(function(error, data) {
+//      meter.transition().delay(250).attr("transform", "scale(0)");
+      f(error, data);
+    })
+})
+
+.defer(function(f) { d3.csv("../data/Politics.csv")
+    .on("progress", function() {
+      var i = d3.interpolate(progress, d3.event.loaded / d3.event.total);
+      d3.transition().tween("progress", function() {
+        return function(t) {
+          progress = i(t);
+          background.attr("d", arc.endAngle(twoPi));
+          foreground.attr("d", arc.endAngle(twoPi * progress));
+          text.text(formatPercent(progress));
+        };
+      });
+    })
+    .get(function(error, data) {
+//      meter.transition().delay(250).attr("transform", "scale(0)");
+      f(error, data);
+    })
+})
+
+/*
+.defer(function(f) { d3.csv("../data/Proximity.csv")
     .on("progress", function() {
       var i = d3.interpolate(progress, d3.event.loaded / d3.event.total);
       d3.transition().tween("progress", function() {
@@ -241,27 +361,64 @@ function loadData() {
       f(error, data);
     })
 })
-    
-/*
-.defer(d3.csv("data/WLAN2.csv")
+*/
+         
+.defer(function(f) { d3.csv("../data/RelationshipsFromSurveys.csv")
     .on("progress", function() {
       var i = d3.interpolate(progress, d3.event.loaded / d3.event.total);
       d3.transition().tween("progress", function() {
         return function(t) {
           progress = i(t);
+          background.attr("d", arc.endAngle(twoPi));
           foreground.attr("d", arc.endAngle(twoPi * progress));
           text.text(formatPercent(progress));
         };
       });
     })
     .get(function(error, data) {
-      meter.transition().delay(250).attr("transform", "scale(0)");
+//      meter.transition().delay(250).attr("transform", "scale(0)");
+      f(error, data);
     })
-    )
-*/
+})
+
+.defer(function(f) { d3.csv("../data/SMS.csv")
+    .on("progress", function() {
+      var i = d3.interpolate(progress, d3.event.loaded / d3.event.total);
+      d3.transition().tween("progress", function() {
+        return function(t) {
+          progress = i(t);
+          background.attr("d", arc.endAngle(twoPi));
+          foreground.attr("d", arc.endAngle(twoPi * progress));
+          text.text(formatPercent(progress));
+        };
+      });
+    })
+    .get(function(error, data) {
+//      meter.transition().delay(250).attr("transform", "scale(0)");
+      f(error, data);
+    })
+})
+         
+.defer(function(f) { d3.csv("../data/WLAN2.csv")
+    .on("progress", function() {
+      var i = d3.interpolate(progress, d3.event.loaded / d3.event.total);
+      d3.transition().tween("progress", function() {
+        return function(t) {
+          progress = i(t);
+          background.attr("d", arc.endAngle(twoPi));
+          foreground.attr("d", arc.endAngle(twoPi * progress));
+          text.text(formatPercent(progress));
+        };
+      });
+    })
+    .get(function(error, data) {
+//      meter.transition().delay(250).attr("transform", "scale(0)");
+      f(error, data);
+    })
+})
     
+
     
-//   defer(function(f) { d3.csv(...).on(...).get(function(error, data) { meter...; f(error, data); }) })
          
   .await(transformData);
 }
